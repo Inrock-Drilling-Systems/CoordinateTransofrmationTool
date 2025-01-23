9072ec3ee7a9f67a59a3a5e2366195c52c49e7aa
Thomas Kyle Robertson <kyle.robertson@inrock.com>
2025-01-23 07:24:23 -0600

    Start and update v0.3

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,10 @@
 # src/__init__.py
-from .constants import spsc83_zones
+from .constants import spsc83_zones, spcs83_to_epsg
 from .create_map import create_map
 from .generate_kmz import generate_kmz
 from .transformations import transform_coordinates
 from .remove_and_reformat import process_survey_csv
 
-__all__ = ['spsc83_zones', 'create_map', 'generate_kmz', 'transform_coordinates', 'process_survey_csv']
+
+__all__ = ['spsc83_zones', 'spcs83_to_epsg', 'create_map', 'generate_kmz', 'transform_coordinates',
+           'process_survey_csv']
