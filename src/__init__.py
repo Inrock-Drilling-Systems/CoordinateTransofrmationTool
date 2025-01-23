ad83a254ecf1dae1aec14a26892c1d39e5bf1840
Thomas Kyle Robertson <kyle.robertson@inrock.com>
2025-01-22 19:44:34 -0600

    Playing with EPSG Codes and Transformations again

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,8 @@
 # src/__init__.py
-from .constants import STATE_PLANE_EPSG_CODES
+from .constants import STATE_PLANE_ZONES
 from .create_map import create_map
 from .generate_kmz import generate_kmz
 from .transformations import transform_coordinates
 from .remove_and_reformat import process_survey_csv
 
-__all__ = ['STATE_PLANE_EPSG_CODES', 'create_map', 'generate_kmz','transform_coordinates','process_survey_csv']
+__all__ = ['STATE_PLANE_ZONES', 'create_map', 'generate_kmz', 'transform_coordinates', 'process_survey_csv']
