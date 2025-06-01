2b8fbfa93e27885734f3ca382ec97f362154d13f
Thomas Kyle Robertson <kyle.robertson@inrock.com>
2025-05-31 19:36:11 -0500

    Added State Plane Lookup page with dummy data. Need to expand

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Inrock Guidance",
     page_icon="🌍",
-    layout="centered" # or "wide" if you prefer
+    layout="wide" # or "wide" if you prefer
 )
 
 st.title("Welcome to the Inrock Transformation Tool")
@@ -15,10 +15,12 @@ st.write(
     This application allows you to perform 3D coordinate transformations
     and visualize survey data.
 
-    **To get started, please select 'CoordConverter' from the sidebar on the left.**
+    **To get started, please select 'Survey Export Transformations' from the sidebar on the left.**
     """
 )
 
-st.info("Need to transform coordinates? Navigate to the 'CoordConverter' page.")
+st.info("Need to transform coordinates? Navigate to the 'Survey Export Transformations' page.")
+
+st.info("Need to find what zone to use? Navigate to the 'State Plane Lookup' page.")
 
 # You can add more content here, like an 'About' section, instructions, or a project overview.
